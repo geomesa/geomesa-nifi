a9505a1c1d80e3d8cb13dd691c2f8f04ee1768db
Wil Selwood <wselwood@gmail.com>
2017-07-21 16:31:23 +0100

    Add Custom SFT and Converter Validators (#25)

---
+++ geomesa-nifi-processors/src/main/scala/org/geomesa/nifi/geo/validators/ConverterValidator.scala
@@ -0,0 +1,16 @@
+package org.geomesa.nifi.geo.validators
+
+import org.apache.nifi.components.{ValidationContext, ValidationResult, Validator}
+import org.locationtech.geomesa.convert.{ConfArgs, ConverterConfigResolver}
+
+/**
+  * Simple validator of a convert spec. Tries to parse it to make sure that it can.
+  */
+object ConverterValidator extends Validator{
+  override def validate(subject: String, input: String, context: ValidationContext): ValidationResult = {
+    ConverterConfigResolver.getArg(ConfArgs(input)) match {
+      case Left(_) => new ValidationResult.Builder().subject(subject).explanation(s"'$subject' is not a valid converter").input(input).build;
+      case Right(_) => new ValidationResult.Builder().subject(subject).input(input).valid(true).build;
+    }
+  }
+}
---
+++ geomesa-nifi-processors/src/main/scala/org/geomesa/nifi/geo/validators/SimpleFeatureTypeValidator.scala
@@ -0,0 +1,16 @@
+package org.geomesa.nifi.geo.validators
+
+import org.apache.nifi.components.{ValidationContext, ValidationResult, Validator}
+import org.locationtech.geomesa.utils.geotools.{SftArgResolver, SftArgs}
+
+/**
+  * Simple validator that tries to parse a given simple feature type to check that it is valid.
+  */
+object SimpleFeatureTypeValidator extends Validator {
+  override def validate(subject: String, input: String, validationContext: ValidationContext): ValidationResult = {
+    SftArgResolver.getArg(SftArgs(input, null)) match {
+      case Left(_) => new ValidationResult.Builder().subject(subject).explanation(s"'$subject' is not a valid simple feature type").input(input).build;
+      case Right(_) => new ValidationResult.Builder().subject(subject).input(input).valid(true).build;
+    }
+  }
+}
