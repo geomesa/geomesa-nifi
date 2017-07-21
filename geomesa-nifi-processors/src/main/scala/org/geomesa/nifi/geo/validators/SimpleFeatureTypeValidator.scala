560224bf69cb6979ed095ef21ae8427b7aec8f1e
Andrew Hulbert <ahulbert@ccri.com>
2017-07-21 11:48:23 -0400

    Line wrapping and format

--- geomesa-nifi-processors/src/main/scala/org/geomesa/nifi/geo/validators/SimpleFeatureTypeValidator.scala
+++ geomesa-nifi-processors/src/main/scala/org/geomesa/nifi/geo/validators/SimpleFeatureTypeValidator.scala
@@ -7,10 +7,12 @@ import org.locationtech.geomesa.utils.geotools.{SftArgResolver, SftArgs}
   * Simple validator that tries to parse a given simple feature type to check that it is valid.
   */
 object SimpleFeatureTypeValidator extends Validator {
-  override def validate(subject: String, input: String, validationContext: ValidationContext): ValidationResult = {
+  override def validate(subject: String, input: String, validationContext: ValidationContext): ValidationResult =
     SftArgResolver.getArg(SftArgs(input, null)) match {
-      case Left(_) => new ValidationResult.Builder().subject(subject).explanation(s"'$subject' is not a valid simple feature type").input(input).build;
-      case Right(_) => new ValidationResult.Builder().subject(subject).input(input).valid(true).build;
+      case Left(_) =>
+        new ValidationResult.Builder().subject(subject)
+          .explanation(s"'$subject' is not a valid simple feature type").input(input).build;
+      case Right(_) =>
+        new ValidationResult.Builder().subject(subject).input(input).valid(true).build;
     }
-  }
 }
