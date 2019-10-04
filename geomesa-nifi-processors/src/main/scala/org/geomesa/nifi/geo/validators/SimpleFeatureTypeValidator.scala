1e0a51e58a3d133835f4b0319dab9ed075710515
Emilio <elahrvivaz@ccri.com>
2019-10-04 17:14:46 -0400

    GEOMESA-2712 Fix classpath for loading config files from s3 (#51)
    Signed-off-by: Emilio Lahr-Vivaz <elahrvivaz@ccri.com>

--- geomesa-nifi-processors/src/main/scala/org/geomesa/nifi/geo/validators/SimpleFeatureTypeValidator.scala
+++ geomesa-nifi-processors/src/main/scala/org/geomesa/nifi/geo/validators/SimpleFeatureTypeValidator.scala
@@ -7,12 +7,11 @@ import org.locationtech.geomesa.utils.geotools.{SftArgResolver, SftArgs}
   * Simple validator that tries to parse a given simple feature type to check that it is valid.
   */
 object SimpleFeatureTypeValidator extends Validator {
-  override def validate(subject: String, input: String, validationContext: ValidationContext): ValidationResult =
+  override def validate(subject: String, input: String, validationContext: ValidationContext): ValidationResult = {
+    val builder = new ValidationResult.Builder().subject(subject).input(input)
     SftArgResolver.getArg(SftArgs(input, null)) match {
-      case Left(_) =>
-        new ValidationResult.Builder().subject(subject)
-          .explanation(s"'$subject' is not a valid simple feature type").input(input).build;
-      case Right(_) =>
-        new ValidationResult.Builder().subject(subject).input(input).valid(true).build;
+      case Left(e)  => builder.explanation(s"'$subject' is not a valid simple feature type: $e").build();
+      case Right(_) => builder.valid(true).build();
     }
+  }
 }
