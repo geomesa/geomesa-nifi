9a5cfa4573ad6c6bc05abffbc0b11e4585a33749
Emilio Lahr-Vivaz <elahrvivaz@ccri.com>
2020-05-25 14:50:19 -0400

    GEOMESA-2852 Separate out HBase nars, refactor module layouts
    Signed-off-by: Emilio Lahr-Vivaz <elahrvivaz@ccri.com>

---
+++ geomesa-datastore-bundle/geomesa-datastore-processors/src/test/scala/org/geomesa/nifi/datastore/processor/validators/TestSimpleFeatureTypeValidator.scala
@@ -0,0 +1,50 @@
+/***********************************************************************
+ * Copyright (c) 2015-2020 Commonwealth Computer Research, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Apache License, Version 2.0
+ * which accompanies this distribution and is available at
+ * http://www.opensource.org/licenses/apache2.0.php.
+ ***********************************************************************/
+
+package org.geomesa.nifi.datastore.processor.validators
+
+import org.apache.nifi.components.ValidationContext
+import org.junit.Assert._
+import org.junit.Test
+import org.mockito.Mockito._
+
+class TestSimpleFeatureTypeValidator {
+
+  private val validInputs = List(
+    "geomesa {sfts {twitter = {fields = [{name = text, type = String}{name = username, type = String}{name = geom, type = Point, srid = 4326}]}}}",
+    "geomesa { sfts { twitter = {fields = []}} }"
+  )
+
+  private val invalidInputs = List(
+    "",
+    "dsjhgjkdsfhgkjfdshgjisfkh",
+    "geomesa { sfts {} }",
+    "geomesa { sfts { twitter = {}} }"
+  )
+
+  @Test
+  def validTests() : Unit = {
+    val c = mock(classOf[ValidationContext])
+
+    validInputs.foreach{ s =>
+      val result = SimpleFeatureTypeValidator.validate("testThing", s, c)
+      assertTrue("\"" + s + "\" should have been valid" , result.isValid)
+    }
+  }
+
+  @Test
+  def invalidTests() : Unit = {
+    val c = mock(classOf[ValidationContext])
+
+    invalidInputs.foreach{ s =>
+      val result = SimpleFeatureTypeValidator.validate("testThing", s, c)
+      assertFalse("\"" + s + "\" should have been invalid" , result.isValid)
+    }
+  }
+
+}
