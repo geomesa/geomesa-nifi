12f9a02528d1c112db3df2fbe487829ee8285328
Emilio Lahr-Vivaz <elahrvivaz@ccri.com>
2022-04-13 08:29:01 -0400

    GEOMESA-3189 Add metrics reporter configuration to ingest

---
+++ geomesa-datastore-bundle/geomesa-datastore-processors/src/main/scala/org/geomesa/nifi/datastore/processor/validators/ConverterMetricsValidator.scala
@@ -0,0 +1,40 @@
+/***********************************************************************
+ * Copyright (c) 2015-2022 Commonwealth Computer Research, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Apache License, Version 2.0
+ * which accompanies this distribution and is available at
+ * http://www.opensource.org/licenses/apache2.0.php.
+ ***********************************************************************/
+
+package org.geomesa.nifi.datastore.processor.validators
+
+import com.codahale.metrics.MetricRegistry
+import com.typesafe.config.ConfigFactory
+import org.apache.nifi.components.{ValidationContext, ValidationResult, Validator}
+import org.geomesa.nifi.datastore.processor.mixins.ConvertInputProcessor
+import org.locationtech.geomesa.metrics.core.ReporterFactory
+
+import scala.util.control.NonFatal
+
+/**
+  * Simple validator of a convert spec. Tries to parse it to make sure that it can.
+  */
+object ConverterMetricsValidator extends Validator {
+
+  import scala.collection.JavaConverters._
+
+  private val DisableStart = ConfigFactory.parseString("""{interval:"-1 seconds"}""")
+
+  override def validate(subject: String, input: String, context: ValidationContext): ValidationResult = {
+    try {
+      val registry = new MetricRegistry()
+      val reporters = ConvertInputProcessor.parseReporterOptions(input).getConfig("options").getConfigList("reporters")
+      reporters.asScala.foreach(conf => ReporterFactory(DisableStart.withFallback(conf), registry).close())
+      new ValidationResult.Builder().subject(subject).input(input).valid(true).build()
+    } catch {
+      case NonFatal(e) =>
+        new ValidationResult.Builder().subject(subject)
+            .explanation(s"'$subject' is not a valid metric reporter: $e").input(input).build()
+    }
+  }
+}
