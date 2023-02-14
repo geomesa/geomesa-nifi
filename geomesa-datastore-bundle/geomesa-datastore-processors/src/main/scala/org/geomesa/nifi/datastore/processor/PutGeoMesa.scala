41930c77cdfe2c75b7c2384976db052a1e488e8b
Emilio Lahr-Vivaz <elahrvivaz@ccri.com>
2023-02-14 17:04:15 -0500

    GEOMESA-3263 Add initialize schema option to processors

--- geomesa-datastore-bundle/geomesa-datastore-processors/src/main/scala/org/geomesa/nifi/datastore/processor/PutGeoMesa.scala
+++ geomesa-datastore-bundle/geomesa-datastore-processors/src/main/scala/org/geomesa/nifi/datastore/processor/PutGeoMesa.scala
@@ -11,15 +11,19 @@ package org.geomesa.nifi.datastore.processor
 import org.apache.nifi.annotation.behavior.InputRequirement.Requirement
 import org.apache.nifi.annotation.behavior._
 import org.apache.nifi.annotation.documentation.{CapabilityDescription, Tags}
-import org.apache.nifi.components.PropertyDescriptor
+import org.apache.nifi.components.{PropertyDescriptor, ValidationContext, ValidationResult, Validator}
+import org.apache.nifi.context.PropertyContext
+import org.apache.nifi.expression.ExpressionLanguageScope
 import org.apache.nifi.flowfile.FlowFile
 import org.apache.nifi.processor._
 import org.geomesa.nifi.datastore.processor.CompatibilityMode.CompatibilityMode
 import org.geomesa.nifi.datastore.processor.mixins.ConvertInputProcessor.ConverterCallback
 import org.geomesa.nifi.datastore.processor.mixins.{ConvertInputProcessor, DataStoreIngestProcessor, FeatureWriters}
 import org.geotools.data._
-import org.opengis.feature.simple.SimpleFeature
+import org.locationtech.geomesa.utils.geotools.{SftArgResolver, SftArgs}
+import org.opengis.feature.simple.{SimpleFeature, SimpleFeatureType}
 
+import java.io.StringReader
 import scala.util.control.NonFatal
 
 
@@ -42,16 +46,21 @@ import scala.util.control.NonFatal
 @SupportsBatching
 class PutGeoMesa extends DataStoreIngestProcessor with ConvertInputProcessor {
 
+  import PutGeoMesa.Properties.InitSchemas
+
   override protected def createIngest(
       context: ProcessContext,
       dataStore: DataStore,
       writers: FeatureWriters,
       mode: CompatibilityMode): IngestProcessor = {
-    new ConverterIngest(dataStore, writers, mode)
+    val ingest = new ConverterIngest(dataStore, writers, mode)
+    // due to validation, should be all Rights
+    ingest.init(PutGeoMesa.initSchemas(context).map { case Right(sft) => sft })
+    ingest
   }
 
   override protected def getTertiaryProperties: Seq[PropertyDescriptor] =
-    Seq(ExtraClasspaths) ++ super.getTertiaryProperties
+    Seq(ExtraClasspaths, InitSchemas) ++ super.getTertiaryProperties
 
   /**
    * Converter ingest
@@ -62,6 +71,8 @@ class PutGeoMesa extends DataStoreIngestProcessor with ConvertInputProcessor {
    */
   class ConverterIngest(store: DataStore, writers: FeatureWriters, mode: CompatibilityMode)
       extends IngestProcessor(store, writers, mode) {
+
+    def init(sfts: Seq[SimpleFeatureType]): Unit = sfts.foreach(checkSchema)
 
     override def ingest(
         context: ProcessContext,
@@ -88,3 +99,50 @@ class PutGeoMesa extends DataStoreIngestProcessor with ConvertInputProcessor {
   }
 }
 
+object PutGeoMesa {
+
+  import scala.collection.JavaConverters._
+
+  object Properties {
+    val InitSchemas: PropertyDescriptor =
+      new PropertyDescriptor.Builder()
+          .name("InitializeSchemas")
+          .required(false)
+          .description(
+            "Initialize schemas in the underlying data store when the processor is started. Schemas should be " +
+                "defined in standard Java properties format, with the type name as the key, and the feature type " +
+                "specification or lookup as the value")
+          .expressionLanguageSupported(ExpressionLanguageScope.VARIABLE_REGISTRY)
+          .addValidator(new InitSchemaValidator())
+          .build()
+  }
+
+  private def initSchemas(context: PropertyContext): Seq[Either[String, SimpleFeatureType]] = {
+    val string = context.getProperty(Properties.InitSchemas).evaluateAttributeExpressions().getValue
+    if (string == null || string.isEmpty) { Seq.empty } else {
+      val props = new java.util.Properties()
+      props.load(new StringReader(string))
+      props.asScala.toSeq.map { case (name, spec) =>
+        SftArgResolver.getArg(SftArgs(spec, name)).left.map(e => s"$name=$spec ${e.getMessage}")
+      }
+    }
+  }
+
+  class InitSchemaValidator extends Validator {
+    override def validate(subject: String, input: String, context: ValidationContext): ValidationResult = {
+      try {
+        val errors = initSchemas(context).collect { case Left(e) => e }
+        if (errors.isEmpty) {
+          new ValidationResult.Builder().subject(subject).input(input).valid(true).build()
+        } else {
+          val exp = errors.mkString("the following feature types could not be loaded: -- ", " -- ", "")
+          new ValidationResult.Builder().subject(subject).input(input).explanation(exp).build()
+        }
+      } catch {
+        case NonFatal(e) =>
+          val exp = s"there was an error loading feature types: $e"
+          new ValidationResult.Builder().subject(subject).input(input).explanation(exp).build()
+      }
+    }
+  }
+}
