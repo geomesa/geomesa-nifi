c2bee6921d0153486735bb534144dda72f21a14a
Emilio Lahr-Vivaz <elahrvivaz@ccri.com>
2024-10-10 15:50:38 -0400

    Update run-nifi script to automatically set the correct data store for ingest

--- build/run-nifi.sh
+++ build/run-nifi.sh
@@ -45,16 +45,17 @@ while getopts ":v:" opt; do
   esac
 done
 
-if [[ -z "$1" ]]; then
+backend="$1"
+if [[ -z "$backend" ]]; then
   usage
   exit 1
 fi
 
-nar="$(find "$dir"/geomesa-* -name "geomesa-$1*.nar")"
+nar="$(find "$dir"/geomesa-* -name "geomesa-$backend*.nar")"
 datastoreNar="$(find "$dir"/geomesa-datastore-bundle/geomesa-datastore-services-nar/target -name "geomesa*.nar")"
 servicesApiNar="$(find "$dir"/geomesa-datastore-bundle/geomesa-datastore-services-api-nar/target -name "geomesa*.nar")"
 
-checkNar "$nar" "$1"
+checkNar "$nar" "$backend"
 checkNar "$datastoreNar" "datastore-services"
 checkNar "$servicesApiNar" "datastore-services-api"
 
@@ -65,15 +66,38 @@ if ! docker network ls | grep -q geomesa; then
   docker network create geomesa
 fi
 
+# these values are from the controller service UUIDs in the flow.json
+controllerId=""
+if [[ $backend =~ accumulo.* ]]; then
+  controllerId="823a37c5-fda6-3e87-b250-c13d073baeeb"
+elif [[ $backend = 'kafka' ]]; then
+  controllerId="980d4c03-f78b-3b68-a0a2-24c47b87e064"
+  # confluentControllerId="638d3ed8-e9bb-3610-b0be-c5f43e84e008"
+elif [[ $backend = 'fs' ]]; then
+  controllerId="272b191a-e2fe-334a-8d52-c513e05c124f"
+elif [[ $backend = 'gt' ]]; then
+  controllerId="fbd8d02b-283c-3362-9d96-8b350af2d630"
+elif [[ $backend = 'hbase' ]]; then
+  controllerId="c18303ac-76ab-3499-a4d0-dbbdf59a52d3"
+elif [[ $backend = 'redis' ]]; then
+  controllerId="207c8fa2-06b4-372f-9cee-f7baa154a6ea"
+fi
+
+if [[ -n "$controllerId" ]]; then
+  sed -i "s/\"DataStoreService\": \".*\"/\"DataStoreService\": \"${controllerId}\"/" "$dir/build/docker/flow.json"
+else
+  echo "WARN: No controller specified for $backend"
+fi
+
 echo "Running NiFi $nifiVersion with $(basename "$nar")..."
 echo ""
 
 docker run --rm \
   --name nifi \
   --network geomesa \
-  -p 8080:8080 -p 8000:8000 \
+  -p 8081:8081 -p 8000:8000 \
   -e NIFI_WEB_HTTP_HOST=0.0.0.0 \
-  -e NIFI_WEB_HTTP_PORT=8080 \
+  -e NIFI_WEB_HTTP_PORT=8081 \
   -e SINGLE_USER_CREDENTIALS_USERNAME=nifi \
   -e SINGLE_USER_CREDENTIALS_PASSWORD=nifipassword \
   -e NIFI_SENSITIVE_PROPS_KEY=supersecretkey \
