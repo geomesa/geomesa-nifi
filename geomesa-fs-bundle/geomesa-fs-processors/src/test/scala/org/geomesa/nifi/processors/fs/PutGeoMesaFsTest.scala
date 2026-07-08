253b0d8ed09cd170911d5452dabd4deb78952ab3
Emilio Lahr-Vivaz <elahrvivaz@ccri.com>
2026-07-08 09:36:06 -0400

    GEOMESA-3590 FSDS - Use Iceberg for metadata

--- geomesa-fs-bundle/geomesa-fs-processors/src/test/scala/org/geomesa/nifi/processors/fs/PutGeoMesaFsTest.scala
+++ geomesa-fs-bundle/geomesa-fs-processors/src/test/scala/org/geomesa/nifi/processors/fs/PutGeoMesaFsTest.scala
@@ -12,23 +12,67 @@ import com.typesafe.scalalogging.LazyLogging
 import org.apache.nifi.util.TestRunners
 import org.geomesa.nifi.datastore.processor.mixins.{ConvertInputProcessor, DataStoreProcessor, FeatureTypeProcessor, UserDataProcessor}
 import org.geomesa.nifi.datastore.processor.{PutGeoMesa, Relationships}
+import org.geomesa.nifi.processors.fs.PutGeoMesaFsTest.IcebergRestContainer
 import org.geotools.api.data.DataStoreFinder
-import org.junit.{Assert, Test}
 import org.locationtech.geomesa.fs.data.FileSystemDataStoreParams
 import org.locationtech.geomesa.utils.collection.CloseableIterator
-import org.locationtech.geomesa.utils.io.{PathUtils, WithClose}
+import org.locationtech.geomesa.utils.io.WithClose
+import org.specs2.mutable.SpecificationWithJUnit
+import org.specs2.specification.BeforeAfterAll
+import org.testcontainers.containers.{GenericContainer, MinIOContainer, Network}
+import org.testcontainers.utility.DockerImageName
 
-import java.nio.file.{Files, Path}
-
-class PutGeoMesaFsTest extends LazyLogging {
+class PutGeoMesaFsTest extends SpecificationWithJUnit with BeforeAfterAll with LazyLogging {
 
   import scala.collection.JavaConverters._
 
-  @Test
-  def testIngest(): Unit = {
-    withDir { dir =>
-      val path = dir.toFile.getAbsolutePath
-      val params = Map(FileSystemDataStoreParams.PathParam.key -> path, FileSystemDataStoreParams.MetadataTypeParam.key -> "file")
+  private val network = Network.newNetwork()
+
+  private val minio =
+    new MinIOContainer(DockerImageName.parse("minio/minio").withTag(sys.props("minio.docker.tag")))
+      .withNetwork(network)
+      .withNetworkAliases("minio")
+
+  private val iceberg =
+    new IcebergRestContainer()
+      .withNetwork(network)
+      .withNetworkAliases("rest-catalog")
+      .withEnv("CATALOG_WAREHOUSE", "s3://geomesa/iceberg")
+      .withEnv("CATALOG_IO__IMPL", "org.apache.iceberg.aws.s3.S3FileIO")
+      .withEnv("CATALOG_S3_ENDPOINT", "http://minio:9000")
+      .withEnv("CATALOG_S3_PATH__STYLE__ACCESS", "true")
+      .withEnv("AWS_REGION", "us-east-1")
+      .withEnv("AWS_ACCESS_KEY_ID", "minioadmin")
+      .withEnv("AWS_SECRET_ACCESS_KEY", "minioadmin")
+
+  override def beforeAll(): Unit = {
+    minio.start()
+    minio.execInContainer("mc", "alias", "set", "localhost", "http://localhost:9000", minio.getUserName, minio.getPassword)
+    minio.execInContainer("mc", "mb", "localhost/geomesa")
+    iceberg.start()
+  }
+
+  override def afterAll(): Unit = {
+    iceberg.stop()
+    minio.stop()
+  }
+
+  "PutGeoMesaFs" should {
+    "ingest" in {
+      val params =
+        Map(
+          FileSystemDataStoreParams.PathParam.key -> "s3://geomesa/fs/",
+          FileSystemDataStoreParams.ConfigParam.key ->
+            s"""type=rest
+               |uri=http://${iceberg.getHost}:${iceberg.getFirstMappedPort}/
+               |iceberg.namespace=geomesa
+               |fs.s3.region=us-east-1
+               |fs.s3.endpoint=${minio.getS3URL}
+               |fs.s3.access-key-id=${minio.getUserName}
+               |fs.s3.secret-access-key=${minio.getPassword}
+               |fs.s3.force-path-style=true
+               |""".stripMargin,
+        )
       val runner = TestRunners.newTestRunner(new PutGeoMesa())
       try {
         val service = new FileSystemDataStoreService()
@@ -41,7 +85,7 @@ class PutGeoMesaFsTest extends LazyLogging {
         runner.setProperty(DataStoreProcessor.Properties.DataStoreService, "fs-datastore")
         runner.setProperty(FeatureTypeProcessor.Properties.SftNameKey, "example")
         runner.setProperty(ConvertInputProcessor.Properties.ConverterNameKey, "example-csv")
-        runner.setProperty(UserDataProcessor.Properties.SftUserData, "geomesa.fs.encoding=parquet\ngeomesa.fs.scheme={name=\"daily,z2-2bit\",options={}}")
+        runner.setProperty(UserDataProcessor.Properties.SftUserData, "geomesa.fs.scheme=daily,z2:bits=4")
         runner.enqueue(getClass.getClassLoader.getResourceAsStream("example.csv"))
         runner.run()
         runner.assertTransferCount(Relationships.SuccessRelationship, 1)
@@ -50,20 +94,22 @@ class PutGeoMesaFsTest extends LazyLogging {
         runner.shutdown()
       }
       WithClose(DataStoreFinder.getDataStore(params.asJava)) { ds =>
-        Assert.assertNotNull(ds)
+        ds must not(beNull)
         val sft = ds.getSchema("example")
-        Assert.assertNotNull(sft)
+        sft must not(beNull)
         val features = CloseableIterator(ds.getFeatureSource("example").getFeatures.features()).toList
         logger.debug(features.mkString(";"))
-        Assert.assertEquals(3, features.length)
+        features must haveLength(3)
       }
     }
   }
+}
 
-  def withDir[T](fn: Path => T): T = {
-    val dir = Files.createTempDirectory("gm-nifi-fs")
-    try { fn(dir) } finally {
-      PathUtils.deleteRecursively(dir)
-    }
+object PutGeoMesaFsTest {
+
+  val IcebergRestImage = DockerImageName.parse("apache/iceberg-rest-fixture").withTag(sys.props("iceberg.rest.docker.tag"))
+
+  class IcebergRestContainer extends GenericContainer[IcebergRestContainer](IcebergRestImage) {
+    withExposedPorts(8181)
   }
 }
